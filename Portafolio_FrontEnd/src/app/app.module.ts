a742935a56b29e9054acca9b7567e8d77d92f76a
FexGH <fenix_el_mejor8130@hotmail.com>
2023-05-15 16:33:05 -0300

    version1 trabajos y educacion

--- Portafolio_FrontEnd/src/app/app.module.ts
+++ Portafolio_FrontEnd/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { SocialwebsLinkComponent } from './components/socialwebs-link/socialwebs
 import { BannerComponent } from './components/banner/banner.component';
 import { AboutmeComponent } from './components/aboutme/aboutme.component';
 import { ExperienceComponent } from './components/experience/experience.component';
+import { EducationComponent } from './components/education/education.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { ExperienceComponent } from './components/experience/experience.componen
     SocialwebsLinkComponent,
     BannerComponent,
     AboutmeComponent,
-    ExperienceComponent
+    ExperienceComponent,
+    EducationComponent
   ],
   imports: [
     BrowserModule,
