3478e24b8357dc21faeee6c44cca9f06c73025f1
FexGH <fenix_el_mejor8130@hotmail.com>
2023-05-15 10:52:00 -0300

    my new header :D

--- Portafolio_FrontEnd/src/app/app.module.ts
+++ Portafolio_FrontEnd/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
+import { SocialwebsLinkComponent } from './components/socialwebs-link/socialwebs-link.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoAPComponent,
+    SocialwebsLinkComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
